a261fa2ce47d1f40518de28489eae7fcb3c3b745
LothaireNobili <lothairenobili@gmail.com>
2023-12-21 14:42:26 +0100

    fixed fight loading issue

--- src/pages/hameau/composerEquipe.js
+++ src/pages/hameau/composerEquipe.js
@@ -62,7 +62,7 @@ class ComposerEquipe extends Phaser.Scene {
       if(go){
         message.setVisible(false);
         game.scene.stop("ComposerEquipe")
-        game.scene.start('Salle');
+        game.scene.start('bootFight');
         document.body.style.cursor = "default";
       }
     });
